2fe2bd5353a308e35bb1c4ded22ecb5d5d224e07
辛京爵 <xinjingjue@ganweisoft.com>
2025-01-25 14:54:20 +0800

    feat:统一Api返回

---
+++ src/Si.CoreHub/OperateResult/Result.cs
@@ -0,0 +1,44 @@
+﻿namespace Si.CoreHub.OperateResult
+{
+    public class Result
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+        public int? PageIndex { get; set; }
+        public int? PageSize { get; set; }
+        public int? TotalCount { get; set; }
+
+        public static Result Successed<T>(T data, string message = "请求成功")
+        {
+            return new Result()
+            {
+                Success = true,
+                Message = message,
+                Data = data
+            };
+        }
+        public static Result Failed(string message = "请求失败")
+        {
+            return new Result()
+            {
+                Success = false,
+                Message = message,
+                Data = null
+            };
+        }
+
+        public static Result SuccessedByPage<T>(T data, int pageIndex, int pageSize, int totalCount, string message = "请求成功")
+        {
+            return new Result()
+            {
+                Success = true,
+                Message = message,
+                Data = data,
+                PageIndex = pageIndex,
+                PageSize = pageSize,
+                TotalCount = totalCount
+            };
+        }
+    }
+}
